3af2c111391d6dc71c7f299197c485ac5a079b2f
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2023-08-18 22:12:53 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# proto actor go / http-server-sample
+
+[protoactor-go](https://github.com/asynkron/protoactor-go) を使った HTTP サーバーのサンプルです。
+
+## 使い方
+
+```sh
+$ cd simple
+$ go run main.go
+```
---
+++ simple/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/asynkron/protoactor-go/actor"
+	"log"
+	"net/http"
+	"time"
+)
+
+type MyActor struct{}
+
+func (state *MyActor) Receive(ctx actor.Context) {
+	switch msg := ctx.Message().(type) {
+	case string:
+		ctx.Respond(fmt.Sprintf("Hello, %s", msg))
+	}
+}
+
+func NewMyActor() actor.Actor {
+	return &MyActor{}
+}
+
+func main() {
+	system := actor.NewActorSystem()
+	props := actor.PropsFromProducer(NewMyActor)
+	myActor := system.Root.Spawn(props)
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			name = "World"
+		}
+		future := system.Root.RequestFuture(myActor, name, 2*time.Second)
+		result, err := future.Result()
+		if err != nil {
+			http.Error(w, "Error occurred", http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, result.(string))
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
